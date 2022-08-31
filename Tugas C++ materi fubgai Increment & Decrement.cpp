91cf47e29bc92c42ad4f136057521932b8b8fc8e
RISKI <Riski180722@gmail.com>
2022-08-31 14:34:47 +0700

    Mengumpulkan tugas C++ materi Increment & Decrement

---
+++ Tugas C++ materi fubgai Increment & Decrement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+   int X = 30;
+
+    cout<<"Nilai Awal X "<<X<<endl;
+    X--;
+    cout<<"Nilai Kedua X "<<X<<endl;
+    X++;
+    X++;
+    cout<<"Nilai Ketiga X "<<X<<endl;
+
+    return 0;
+}
